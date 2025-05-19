08e36adcc7472d3b1e4525ef989c21decc306c04
renchooz <rjsharma.rs967@gmail.com>
2025-05-19 13:31:37 +0530

    added changes

--- controller/userController.js
+++ controller/userController.js
@@ -87,9 +87,9 @@ export const isAuth = async (req, res) => {
 export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
-      httpOnly: true,
+       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     });
     res.json({ status: true, message: "Loged Out succesfully" });
   } catch (error) {
