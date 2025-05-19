25070ad7e8f9bf6413943e09d23b555d31d26db5
renchooz <rjsharma.rs967@gmail.com>
2025-05-19 13:35:13 +0530

    added samesite none

--- controller/userController.js
+++ controller/userController.js
@@ -24,7 +24,7 @@ export const userData = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     
@@ -56,7 +56,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+       sameSite: 'None'
     });
     return res.json({
       status:true,
@@ -89,7 +89,7 @@ export const logout = async (req, res) => {
     res.clearCookie("token", {
        httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+       sameSite: 'None'
     });
     res.json({ status: true, message: "Loged Out succesfully" });
   } catch (error) {
